b985ac9297b36e7782bd0d705b71b243a3c3063e
samedfft2634 <samedfft@gmail.com>
2024-03-07 09:46:49 +0300

    feat: LMS_Model

---
+++ backend/src/models/lms.model.js
@@ -0,0 +1,46 @@
+"use strict";
+/* ====================================================== */
+/*                       LMS - MODEL                      */
+/* ====================================================== */
+
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite:./db.sqlite3");
+
+const Book = sequelize.define("Book", {
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	author: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	ISBN: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	genre: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	publicationYear: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	image: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+});
+
+// Syncronization:
+sequelize.sync(); // CREATE TABLE
+// sequelize.sync({alter:true}); // TO BACKUP & DROP TABLE & CREATE TABLE & FROM BACKUP
+
+// Connect to db:
+sequelize
+	.authenticate()
+	.then(() => console.log("* DB connected *"))
+	.catch(() => console.log("* DB couldn't connected * "));
+
+module.exports = Book;
